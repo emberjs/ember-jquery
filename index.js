7bacdbf5175a670a8278860934338644127fa0fd
cibernox <miguel.camba@gmail.com>
2018-05-23 06:49:10 -0700

    Add jquery in vendor and import it

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-jquery'
+  name: 'ember-jquery',
+  included() {
+    this._super.included.apply(this, arguments);
+    let optionalFeatures = app.project.findAddonByName("@ember/optional-features");
+    let app = this._findHost();
+    app.import('vendor/jquery.js')
+    if (optionalFeatures && optionalFeatures.isFeatureEnabled('jquery-integration')) {
+      app.project.ui.writeDeprecateLine('You have disabled the `jquery-integration` optional feature. You now have to delete `@ember/jquery` from your package.json');
+    }
+  }
 };
