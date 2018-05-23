26f3a35e2028ecbeafcce50101cd0dcf50c88f61
cibernox <miguel.camba@gmail.com>
2018-05-23 07:28:34 -0700

    Ensure jquery is imported before ember itself

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = {
     this._super.included.apply(this, arguments);
     let optionalFeatures = app.project.findAddonByName("@ember/optional-features");
     let app = this._findHost();
-    app.import('vendor/jquery.js')
+    app.import('vendor/jquery.js', { prepend: true });
     if (optionalFeatures && optionalFeatures.isFeatureEnabled('jquery-integration')) {
       app.project.ui.writeDeprecateLine('You have disabled the `jquery-integration` optional feature. You now have to delete `@ember/jquery` from your package.json');
     }
