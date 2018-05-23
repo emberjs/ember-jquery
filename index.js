d5376ec2545809cba7551aec777c7630db5d8828
Robert Jackson <rjackson@linkedin.com>
2018-05-23 18:14:16 -0400

    Fix invalid JS (app not defined).

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ module.exports = {
   name: 'ember-jquery',
   included() {
     this._super.included.apply(this, arguments);
-    let optionalFeatures = app.project.findAddonByName("@ember/optional-features");
     let app = this._findHost();
+    let optionalFeatures = app.project.findAddonByName("@ember/optional-features");
     app.import('vendor/jquery/jquery.js', { prepend: true });
     if (optionalFeatures && optionalFeatures.isFeatureEnabled('jquery-integration')) {
       app.project.ui.writeDeprecateLine('You have disabled the `jquery-integration` optional feature. You now have to delete `@ember/jquery` from your package.json');
