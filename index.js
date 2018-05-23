14e1f5f7f74284ff3e47cd55460d5d06c743fe58
Robert Jackson <rjackson@linkedin.com>
2018-05-23 18:01:06 -0400

    Use jquery npm package to present vendor tree.

--- index.js
+++ index.js
@@ -6,9 +6,31 @@ module.exports = {
     this._super.included.apply(this, arguments);
     let optionalFeatures = app.project.findAddonByName("@ember/optional-features");
     let app = this._findHost();
-    app.import('vendor/jquery.js', { prepend: true });
+    app.import('vendor/jquery/jquery.js', { prepend: true });
     if (optionalFeatures && optionalFeatures.isFeatureEnabled('jquery-integration')) {
       app.project.ui.writeDeprecateLine('You have disabled the `jquery-integration` optional feature. You now have to delete `@ember/jquery` from your package.json');
     }
-  }
+  },
+
+  treeForVendor: function() {
+    const Funnel = require('broccoli-funnel');
+    const resolve = require('resolve');
+    const path = require('path');
+
+    var jqueryPath;
+    try {
+      jqueryPath = path.dirname(
+        resolve.sync('jquery/package.json', { basedir: this.project.root })
+      );
+    } catch (error) {
+      jqueryPath = path.dirname(require.resolve('jquery/package.json'));
+    }
+
+    var jquery = new Funnel(jqueryPath + '/dist', {
+      destDir: 'jquery',
+      files: ['jquery.js'],
+    });
+
+    return jquery;
+  },
 };
