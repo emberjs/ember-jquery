509e6bcbfecff7bd97ac78f88b36a730086879a5
simonihmig <simon.ihmig@kaliber5.de>
2019-01-08 13:36:05 +0100

    ember-cli-update to 3.6
    Update to latest blueprints, with some modifications (always test qith jQuery).
    
    *Breaking:* drops node 4 support.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  name: '@ember/jquery',
+  name: require('./package').name,
   included() {
     this._super.included.apply(this, arguments);
     let app = this._findHost();
