a85b5bd7fe40d94dca5d32f5e8ee13885caeafb3
Matthew Beale <matt.beale@madhatted.com>
2021-06-09 21:42:47 -0400

    Bump several dev dependencies

--- tests/integration/components/component-dot-dollar-test.js
+++ tests/integration/components/component-dot-dollar-test.js
@@ -1,7 +1,7 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import { hbs } from 'ember-cli-htmlbars';
 import Component from '@ember/component';
 import jQuery from 'jquery';
 
